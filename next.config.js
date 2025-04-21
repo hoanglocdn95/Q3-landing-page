136a0411929aa202785cd7348b2e719563651317
Trần Hoàng Lộc <hoanglocdn95@Trans-Laptop.local>
2025-04-21 21:36:22 +0700

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,61 @@
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+                {
+                  name: 'prefixIds',
+                  params: {
+                    prefixIds: true,
+                    prefixClassNames: false,
+                    prefix: (() => {
+                      const uniqueId = Date.now();
+                      let counter = 0;
+                      return () => `icon-${uniqueId}-${counter++}`;
+                    })(),
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 2000000,
+    };
+
+    return config;
+  },
+};
+
+export default nextConfig;
